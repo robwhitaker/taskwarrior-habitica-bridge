be83690e4e5689bf2e289578582582e6fa1b3c2a
Rob Whitaker <dev@robwhitaker.com>
2020-02-15 18:07:10 -0500

    Added lorri support.

--- pinned-packages.nix
+++
@@ -1,9 +0,0 @@
-let
-  pkgs1909 = fetchTarball {
-    url = https://github.com/NixOS/nixpkgs/archive/19.09.tar.gz;
-    sha256 = "0mhqhq21y5vrr1f30qd2bvydv4bbbslvyzclhw0kdxmkgg3z4c92";
-  };
-in
-  {
-    pkgs1909 = import pkgs1909 {};
-  }
--- shell.nix
+++ shell.nix
@@ -1,5 +1,8 @@
 let
-  pkgs = (import ./pinned-packages.nix).pkgs1909;
+  pkgs = import (fetchTarball {
+    url = https://github.com/NixOS/nixpkgs/archive/19.09.tar.gz;
+    sha256 = "0mhqhq21y5vrr1f30qd2bvydv4bbbslvyzclhw0kdxmkgg3z4c92";
+  }) {};
   drv = import ./. { inherit pkgs; };
 in
   pkgs.haskellPackages.shellFor {
